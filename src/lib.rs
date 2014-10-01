f0ec8d9650e9fb159f91d9abed0278ed602c4067
wei yen <lee+github@weiyen.me>
2014-10-02 00:26:46 +1000

    Minor refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,126 +1,35 @@
-use std::collections::HashMap;
-use std::collections::hashmap::{Occupied, Vacant};
-use std::hash::Hash;
+mod sparse_graph;
 
-trait Graph<N>{
-    fn contains_node(&self, node: N) -> bool;
-    fn contains_edge(&self, from: N, to: N) -> bool;
-}
-
-#[deriving(Hash, Eq, PartialEq, Clone)]
-struct Edge<N>{
-    from: N,
-    to: N,
-    weight: i64,
-}
-
-struct SparseGraph<N: Hash+Eq+Clone>( HashMap<N, HashMap<N, Edge<N>>>);
-impl<N: Hash+Eq+Clone> SparseGraph<N>{
-    fn new()->SparseGraph<N>{
-        let map = HashMap::new();
-        SparseGraph(map)
+mod roost{
+    use std::hash::Hash;
+    use std::hash::sip::SipState;
+    use std::iter::
+    pub trait Graph<N>{
+        fn contains_node(&self, node: N) -> bool;
+        fn contains_edge(&self, from: N, to: N) -> bool;
+        fn neighbors(&self, node: N) -> Iterator<N>;
     }
 
-    /*fn add_node(&mut self, n: N){
-        let &SparseGraph(ref mut map) = self;
-        match map.entry(n) {
-            Occupied(_) => {},
-            Vacant(entry) => {
-                let hs: HashMap<N, Edge<N>> = HashMap::new();
-                entry.set(hs);
-            }
-        };
-    }*/
-
-    fn add_edge(&mut self, e: Edge<N>){
-        let from = e.from.clone();
-        let to = e.to.clone();
-        let &SparseGraph(ref mut map) = self;
-        match map.entry(from.clone()) {
-            Occupied(entry) => {
-                let hs = entry.into_mut();
-                hs.insert(to.clone(), e.clone());
-            },
-            Vacant(entry) => {
-                let mut hs = HashMap::new();
-                hs.insert(to.clone(), e.clone());
-                entry.set(hs);
-            }
-        };
-        match map.entry(to) {
-            Occupied(entry) => {
-                let hs = entry.into_mut();
-                hs.insert(from, e.clone());
-            },
-            Vacant(entry) => {
-                let mut hs = HashMap::new();
-                hs.insert(from, e.clone());
-                entry.set(hs);
-            }
-        };
+    #[deriving(PartialEq, Clone)]
+    pub struct Edge<N>{
+        pub from: N,
+        pub to: N,
+        pub weight: f64,
     }
-}
+    
+    // pub type Neighbors<N> = 
+    //     idx: int,
+    //     nodes: Vec<N>,
+    // }
 
-impl<N: Hash+Eq+Clone> FromIterator<Edge<N>> for SparseGraph<N>{
-    fn from_iter<T: Iterator<Edge<N>>>(mut iterator: T) -> SparseGraph<N>{
-        let mut graph:SparseGraph<N> = SparseGraph::new();
-        for edge in iterator{
-            graph.add_edge(edge);
-        }
-        graph
-    }
-}
-
-impl<N: Hash+Eq+Clone> Graph<N> for SparseGraph<N> {
-    fn contains_node(&self, node: N) -> bool{
-        let &SparseGraph(ref map) = self;
-        map.contains_key(&node)
-    }
-
-    fn contains_edge(&self, from: N, to: N) -> bool{
-        let &SparseGraph(ref map) = self;
-        match map.find(&from){
-            Some(entry) => entry.contains_key(&to),
-            None => false
+    impl<N:Hash> Hash for Edge<N> {
+        fn hash(&self, state: &mut SipState) {
+            self.from.hash(state);
+            self.to.hash(state);
         }
     }
-}
 
-#[test]
-fn new_graph_with_str_nodes(){
-    let edges = vec![
-        Edge{from: "A", to: "B", weight: 1},
-        Edge{from: "A", to: "C", weight: 2},
-        Edge{from: "B", to: "C", weight: 1},
-        Edge{from: "C", to: "D", weight: 1},
-    ];
-    let gp:SparseGraph<&str> = edges.into_iter().collect();
-    assert!(gp.contains_node("A"));
-    assert!(gp.contains_node("B"));
-    assert!(gp.contains_node("C"));
-    assert!(gp.contains_node("D"));
-    assert!(!gp.contains_node("E"));
-
-    assert!(gp.contains_edge("A", "B"));
-    assert!(gp.contains_edge("B", "A")); //Simple undirected graph
-    assert!(!gp.contains_edge("A", "D"));
-}
-
-#[test]
-fn new_graph_with_int_nodes(){
-    let edges = vec![
-        Edge{from: 1i, to: 2, weight: 1},
-        Edge{from: 1i, to: 3, weight: 2},
-        Edge{from: 2i, to: 3, weight: 1},
-        Edge{from: 3i, to: 4, weight: 1},
-    ];
-    let gp:SparseGraph<int> = edges.into_iter().collect();
-    assert!(gp.contains_node(1));
-    assert!(gp.contains_node(2));
-    assert!(gp.contains_node(3));
-    assert!(gp.contains_node(4));
-    assert!(!gp.contains_node(5));
-
-    assert!(gp.contains_edge(1i, 2));
-    assert!(!gp.contains_edge(1, 4));
+    // impl<N> Iterator<N> for Neighbors<N> {
+    //     fn next(&mut self) -> Option<N> {
+            
 }
---
+++ src/sparse_graph.rs
@@ -0,0 +1,135 @@
+use std::collections::HashMap;
+use std::collections::hashmap::{Occupied, Vacant};
+use std::hash::Hash;
+use std::vec::Vec;
+use roost::{Graph, Edge};
+
+struct SparseGraph<N: Hash+Eq+Clone>( HashMap<N, HashMap<N, Edge<N>>>);
+impl<N: Hash+Eq+Clone> SparseGraph<N>{
+    fn new()->SparseGraph<N>{
+        let map = HashMap::new();
+        SparseGraph(map)
+    }
+
+    fn add_node(&mut self, n: N){
+        let &SparseGraph(ref mut map) = self;
+        match map.entry(n) {
+            Occupied(_) => {},
+            Vacant(entry) => {
+                let hs: HashMap<N, Edge<N>> = HashMap::new();
+                entry.set(hs);
+            }
+        };
+    }
+
+    fn add_edge(&mut self, e: Edge<N>){
+        let from = e.from.clone();
+        let to = e.to.clone();
+        let &SparseGraph(ref mut map) = self;
+        match map.entry(from.clone()) {
+            Occupied(entry) => {
+                let hs = entry.into_mut();
+                hs.insert(to.clone(), e.clone());
+            },
+            Vacant(entry) => {
+                let mut hs = HashMap::new();
+                hs.insert(to.clone(), e.clone());
+                entry.set(hs);
+            }
+        };
+        match map.entry(to) {
+            Occupied(entry) => {
+                let hs = entry.into_mut();
+                hs.insert(from, e.clone());
+            },
+            Vacant(entry) => {
+                let mut hs = HashMap::new();
+                hs.insert(from, e.clone());
+                entry.set(hs);
+            }
+        };
+    }
+}
+
+impl<N: Hash+Eq+Clone> FromIterator<Edge<N>> for SparseGraph<N>{
+    fn from_iter<T: Iterator<Edge<N>>>(mut iterator: T) -> SparseGraph<N>{
+        let mut graph:SparseGraph<N> = SparseGraph::new();
+        for edge in iterator{
+            graph.add_edge(edge);
+        }
+        graph
+    }
+}
+
+impl<N: Hash+Eq+Clone> Graph<N> for SparseGraph<N> {
+    fn contains_node(&self, node: N) -> bool{
+        let &SparseGraph(ref map) = self;
+        map.contains_key(&node)
+    }
+
+    fn contains_edge(&self, from: N, to: N) -> bool{
+        let &SparseGraph(ref map) = self;
+        match map.find(&from){
+            Some(entry) => entry.contains_key(&to),
+            None => false
+        }
+    }
+
+    fn neighbors(&self, node:N)->Iterator<N>{
+        let &SparseGraph(ref map) = self;
+        let neighbors = match map.find(&node){
+            Some(entry) => entry.keys(),
+                // let mut res = Vec::new();
+                // for k in entry.keys(){
+                //     res.push(k.clone());
+                // }
+                // res
+            None        => None,
+        };
+        neighbors
+    }
+}
+
+#[test]
+fn new_graph_with_str_nodes(){
+    let edges = vec![
+        Edge{from: "A", to: "B", weight: 1.1},
+        Edge{from: "A", to: "C", weight: 2.3},
+        Edge{from: "B", to: "C", weight: 1.2},
+        Edge{from: "C", to: "D", weight: 1.5},
+    ];
+    let gp:SparseGraph<&str> = edges.into_iter().collect();
+    assert!(gp.contains_node("A"));
+    assert!(gp.contains_node("B"));
+    assert!(gp.contains_node("C"));
+    assert!(gp.contains_node("D"));
+    assert!(!gp.contains_node("E"));
+
+    assert!(gp.contains_edge("A", "B"));
+    assert!(gp.contains_edge("B", "A")); //Simple undirected graph
+    assert!(!gp.contains_edge("A", "D"));
+
+    let mut gp_mut = gp;
+    assert!(!gp_mut.contains_node("E"));
+    gp_mut.add_node("E");
+    assert!(gp_mut.contains_node("E"));
+}
+
+#[test]
+fn new_graph_with_int_nodes(){
+    let edges = vec![
+        Edge{from: 1i, to: 2, weight: 1.0},
+        Edge{from: 1i, to: 3, weight: 2.5},
+        Edge{from: 2i, to: 3, weight: 1.0},
+        Edge{from: 3i, to: 4, weight: 1.0},
+    ];
+    let gp:SparseGraph<int> = edges.into_iter().collect();
+    assert!(gp.contains_node(1));
+    assert!(gp.contains_node(2));
+    assert!(gp.contains_node(3));
+    assert!(gp.contains_node(4));
+    assert!(!gp.contains_node(5));
+
+    assert!(gp.contains_edge(1i, 2));
+    assert!(!gp.contains_edge(1, 4));
+}

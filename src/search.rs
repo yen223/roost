52162fe6d8846e33a2e4ec7104d586e1c4a20540
weiyen <lee+github@weiyen.me>
2014-10-05 03:53:59 +1100

    Added depth-first search

--- src/search.rs
+++ src/search.rs
@@ -3,11 +3,6 @@ use std::collections::{HashSet, RingBuf, Deque};
 use std::hash::Hash;
 use std::iter::Iterator;
 
-// struct DepthFirstVisit<'a, V:Clone+Eq, E, G:'a + Graph<V, E>>{
-//     gp: &'a G,
-//     next_nodes: Vec<NodeIndex>,
-//     visited: HashSet<NodeIndex>,
-// }
 
 struct BreadthFirstVisit<'a, V:Clone+Eq, E, G:'a + Graph<V, E>>{
     gp: &'a G,
@@ -15,11 +10,11 @@ struct BreadthFirstVisit<'a, V:Clone+Eq, E, G:'a + Graph<V, E>>{
     visited: HashSet<NodeIndex>,
 }
 
-// pub fn depth_first_visit<'a, V:Clone+Eq, E, G: Graph<V, E>>(graph: &'a G, root: N)->DepthFirstVisit<'a, V, E, G>{
-//     let mut visit:HashSet<NodeIndex> = HashSet::new();
-//     visit.insert(root.clone());
-//     DepthFirstVisit{gp: graph, next_nodes: vec![root], visited: visit}
-// }
+struct DepthFirstVisit<'a, V:Clone+Eq, E, G:'a + Graph<V, E>>{
+    gp: &'a G,
+    next_nodes: Vec<NodeIndex>,
+    visited: HashSet<NodeIndex>,
+}
 
 pub trait Searchable<V:Clone+Eq,E>:Graph<V,E> {
     fn breadth_first_visit(&self, root: &V)->BreadthFirstVisit<V, E, Self>{
@@ -34,9 +29,21 @@ pub trait Searchable<V:Clone+Eq,E>:Graph<V,E> {
         }
         BreadthFirstVisit{gp: self, next_nodes: queue, visited: visit}
     }
+
+    fn depth_first_visit(&self, root: &V)-> DepthFirstVisit<V, E, Self>{
+        let mut visit:HashSet<NodeIndex> = HashSet::new();
+        let mut stack = Vec::new(); 
+        match self.index_of(root){
+            Some(x) => {
+                visit.insert(x);
+                stack.push(x);
+            },
+            None    => {}
+        }
+        DepthFirstVisit{gp: self, next_nodes: stack, visited: visit}
+    }
 }
 
-impl <V:Clone+Eq, E:Clone> Searchable<V,E> for SparseGraph<V,E>{}
 
 impl<'a, N:Clone+Eq, E, G: Graph<N, E>> Iterator<NodeIndex> for BreadthFirstVisit<'a, N, E, G>{
     fn next(&mut self) -> Option<NodeIndex>{
@@ -56,34 +63,28 @@ impl<'a, N:Clone+Eq, E, G: Graph<N, E>> Iterator<NodeIndex> for BreadthFirstVisi
         curr
     }
 }
-// pub fn breadth_first_visit<'a, N:Clone+Eq, E, G: Graph<N, E>>(graph: &'a G, root: N)->BreadthFirstVisit<'a, N, E, G>{
-//     let mut visit:HashSet<NodeIndex> = HashSet::new();
-//     let root_idx = graph.index_of(root);
-//     visit.insert(root_idx);
-//     let mut queue = RingBuf::new();
-//     queue.push(root_idx);
-//     BreadthFirstVisit{gp: graph, next_nodes: queue, visited: visit}
-// }
 
-// impl<'a, N:Clone+Hash+Eq, G: Graph<N>> Iterator<N> for DepthFirstVisit<'a, N, G>{
-//     fn next(&mut self) -> Option<N>{
-//         let curr = self.next_nodes.pop();
-//         match curr {
-//             Some(ref node) => {
-//                 let neighbors = self.gp.neighbors(node.clone());
-//                 for n in neighbors.into_iter(){
-//                     if !self.visited.contains(&n){
-//                         self.visited.insert(n.clone());
-//                         self.next_nodes.push(n.clone());
-//                     }
-//                 }
-//             },
-//             None => {}
-//         }
-//         curr
-//     }
-// }
+impl<'a, N:Clone+Eq, E, G: Graph<N, E>> Iterator<NodeIndex> for DepthFirstVisit<'a, N, E, G>{
+    fn next(&mut self) -> Option<NodeIndex>{
+        let curr = self.next_nodes.pop();
+        match curr {
+            Some(node) => {
+                let neighbors = self.gp.neighbors(node);
+                for n in neighbors.into_iter(){
+                    if !self.visited.contains(&n){
+                        self.visited.insert(n);
+                        self.next_nodes.push(n);
+                    }
+                }
+            },
+            None => {}
+        }
+        curr
+    }
+}
 
+
+impl <V:Clone+Eq, E:Clone> Searchable<V,E> for SparseGraph<V,E>{}
 
 #[test]
 fn breadth_first_search(){
@@ -94,8 +95,23 @@ fn breadth_first_search(){
     graph.add_edge(2i, 5i, 4);
     graph.add_edge(3i, 6i, 6);
     graph.add_edge(3i, 7i, 7);
-    println!("Breadth first visit:")
-    for idx in graph.breadth_first_visit(&1i){
-        println!("Visited: {}", graph.node_of(idx));
-    }
+    let bfs:Vec<int> = graph.breadth_first_visit(&1i)
+                            .map(|x|{graph.node_of(x).unwrap()})
+                            .collect();
+    assert_eq!(bfs, vec![1, 2, 3, 6, 5, 7]);
+}
+
+#[test]
+fn depth_first_search(){
+    let mut graph:SparseGraph<int, int> = SparseGraph::new();
+    graph.add_edge(1i, 2i, 1);
+    graph.add_edge(1i, 3i, 2);
+    graph.add_edge(1i, 4i, 1);
+    graph.add_edge(4i, 5i, 4);
+    graph.add_edge(4i, 6i, 6);
+    graph.add_edge(5i, 7i, 7);
+    let dfs:Vec<int> = graph.depth_first_visit(&1i)
+                            .map(|x|{graph.node_of(x).unwrap()})
+                            .collect();
+    assert_eq!(dfs, vec![1, 4, 6, 5, 7, 3, 2]);
 }

7af55630df644c19ed5b3a5aaf34d2b19b8ad4ba
weiyen <lee+github@weiyen.me>
2014-10-11 14:35:41 +1100

    BFS and DFS now yields (parent, node) tuples, for easier backtracking.

--- src/search.rs
+++ src/search.rs
@@ -1,7 +1,7 @@
-use roost::{Graph, SparseGraph, NodeIndex};
+use roost::{Graph, SparseGraph, NodeIndex, EdgeIndex};
 use roost::edge::DistanceEdge;
-use std::collections::{HashSet, RingBuf, DList, Deque, PriorityQueue};
-use std::iter::Iterator;
+use std::collections::{HashSet, RingBuf, DList, Deque, PriorityQueue, };
+use std::iter::{Iterator, DoubleEndedIterator};
 
 
 struct BreadthFirstVisit<'a, V, E, G>
@@ -9,7 +9,7 @@ struct BreadthFirstVisit<'a, V, E, G>
           G: 'a + Graph<V,E>,
 {
     gp: &'a G,
-    next_nodes: RingBuf<NodeIndex>,
+    next_edges: DList<EdgeIndex>,
     visited: HashSet<NodeIndex>,
 }
 
@@ -18,7 +18,7 @@ struct DepthFirstVisit<'a, V, E, G>
           G: 'a + Graph<V,E>,
 {
     gp: &'a G,
-    next_nodes: Vec<NodeIndex>,
+    next_edges: Vec<EdgeIndex>,
     visited: HashSet<NodeIndex>,
 }
 
@@ -58,15 +58,17 @@ pub trait Searchable<V,E>:Graph<V,E>
 {
     fn breadth_first_visit(&self, root: &V)->BreadthFirstVisit<V, E, Self>{
         let mut visit:HashSet<NodeIndex> = HashSet::new();
-        let mut queue = RingBuf::new();
+        let mut queue = DList::new();
         match self.index_of(root){
             Some(x) => {
                 visit.insert(x);
-                queue.push(x);
+                for &nb in self.out_nodes(x).iter(){
+                    queue.push((x, nb));
+                }
             },
             None    => {}
         }
-        BreadthFirstVisit{gp: self, next_nodes: queue, visited: visit}
+        BreadthFirstVisit{gp: self, next_edges: queue, visited: visit}
     }
 
     fn depth_first_visit(&self, root: &V)-> DepthFirstVisit<V, E, Self>{
@@ -75,15 +77,17 @@ pub trait Searchable<V,E>:Graph<V,E>
         match self.index_of(root){
             Some(x) => {
                 visit.insert(x);
-                stack.push(x);
+                for &nb in self.out_nodes(x).iter().rev(){
+                    stack.push((x, nb));
+                }
             },
             None    => {}
         }
-        DepthFirstVisit{gp: self, next_nodes: stack, visited: visit}
+        DepthFirstVisit{gp: self, next_edges: stack, visited: visit}
     }
 }
 
-pub trait PathSearchable<N, V, E>: Searchable<V, E> 
+pub trait PathSearchable<N, V, E>: Graph<V, E> 
     where N: Num+ToPrimitive,
           V: Clone+Eq,
           E: DistanceEdge<N>,
@@ -139,19 +143,19 @@ pub trait PathSearchable<N, V, E>: Searchable<V, E>
     }
 }
 
-impl<'a, N, E, G> Iterator<NodeIndex> for BreadthFirstVisit<'a, N, E, G>
+impl<'a, N, E, G> Iterator<EdgeIndex> for BreadthFirstVisit<'a, N, E, G>
     where N: Clone+Eq,
           G: Graph<N, E>,
 {
-    fn next(&mut self) -> Option<NodeIndex>{
-        let curr = self.next_nodes.pop_front();
+    fn next(&mut self) -> Option<EdgeIndex>{
+        let curr = self.next_edges.pop_front();
         match curr {
-            Some(node) => {
+            Some((_, node)) => {
                 let out_nodes = self.gp.out_nodes(node);
                 for n in out_nodes.into_iter(){
                     if !self.visited.contains(&n){
                         self.visited.insert(n);
-                        self.next_nodes.push(n);
+                        self.next_edges.push((node, n));
                     }
                 }
             },
@@ -161,23 +165,22 @@ impl<'a, N, E, G> Iterator<NodeIndex> for BreadthFirstVisit<'a, N, E, G>
     }
 }
 
-impl<'a, N, E, G> Iterator<NodeIndex> for DepthFirstVisit<'a, N, E, G>
+impl<'a, N, E, G> Iterator<EdgeIndex> for DepthFirstVisit<'a, N, E, G>
     where N: Clone+Eq,
           G: Graph<N, E>
 {
-    fn next(&mut self) -> Option<NodeIndex>{
-        let curr = self.next_nodes.pop();
+    fn next(&mut self) -> Option<EdgeIndex>{
+        let curr = self.next_edges.pop();
         match curr {
-            Some(node) => {
+            Some((_, node)) if !self.visited.contains(&node) => {
+                self.visited.insert(node);
                 let out_nodes = self.gp.out_nodes(node);
-                for n in out_nodes.into_iter(){
-                    if !self.visited.contains(&n){
-                        self.visited.insert(n);
-                        self.next_nodes.push(n);
-                    }
+                for n in out_nodes.into_iter().rev(){
+                    self.next_edges.push((node, n));
                 }
             },
-            None => {}
+            Some(_) => {}
+            None    => {}
         }
         curr
     }
@@ -197,10 +200,17 @@ fn breadth_first_search(){
     graph.add_edge(4i, 5i, 4);
     graph.add_edge(4i, 6i, 6);
     graph.add_edge(5i, 7i, 7);
-    let bfs:Vec<int> = graph.breadth_first_visit(&1i)
-                            .map(|x|{graph.node_of(x).unwrap()})
+    let bfs:Vec<(int, int)> = graph.breadth_first_visit(&1i)
+                            .map(|(x,y)|{
+                                (graph.node_of(x).unwrap(), graph.node_of(y).unwrap())
+                            })
                             .collect();
-    assert_eq!(bfs, vec![1, 2, 3, 4, 5, 6, 7]);
+    assert_eq!(bfs, vec![(1i, 2i), 
+                         (1i, 3i), 
+                         (1i, 4i),
+                         (4i, 5i),
+                         (4i, 6i),
+                         (5i, 7i)]);
 }
 
 #[test]
@@ -208,14 +218,22 @@ fn depth_first_search(){
     let mut graph:SparseGraph<int, int> = SparseGraph::new();
     graph.add_edge(1i, 2i, 1);
     graph.add_edge(1i, 3i, 2);
+    graph.add_edge(2i, 3i, 4);
     graph.add_edge(1i, 4i, 1);
-    graph.add_edge(4i, 5i, 4);
-    graph.add_edge(4i, 6i, 6);
+    graph.add_edge(3i, 5i, 4);
+    graph.add_edge(3i, 6i, 6);
     graph.add_edge(5i, 7i, 7);
-    let dfs:Vec<int> = graph.depth_first_visit(&1i)
-                            .map(|x|{graph.node_of(x).unwrap()})
+    let dfs:Vec<(int, int)> = graph.depth_first_visit(&1i)
+                            .map(|(x, y)|{
+                                (graph.node_of(x).unwrap(), graph.node_of(y).unwrap())
+                            })
                             .collect();
-    assert_eq!(dfs, vec![1, 4, 6, 5, 7, 3, 2]);
+    assert_eq!(dfs, vec![(1i, 2i), 
+                         (2i, 3i),
+                         (3i, 5i),
+                         (5i, 7i),
+                         (3i, 6i),
+                         (1i, 4i),]);
 }
 
 #[test]
